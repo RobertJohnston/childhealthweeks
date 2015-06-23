dad4e73a38a128e7f86b3affebca25b675561f53
Robert Johnston <rfjohnstonunicef@gmail.com>
2015-06-23 16:38:08 +0200

    seeded 1st admin names

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,38 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+list_provinces =[
+  [1 , "Bubanza"],
+  [2 , "Bujumbura Rural"],
+  [3 , "Bururi"],
+  [4 , "Cankuzo"],
+  [5 , "Cibitoke"],
+  [6 , "Gitega"],
+  [7 , "Karuzi"],
+  [8 , "Kayanza"],
+  [9 , "Kirundo"],
+  [10  , "Makamba"],
+  [11  , "Muramvya"],
+  [12  , "Muyinga"],
+  [13  , "Mwaro"],
+  [14  , "Ngozi"],
+  [15  , "Rumonge"],
+  [16  , "Rutana"],
+  [17  , "Ruyigi"],
+  [18  , "Mairie de Bujumbura"],
+]
 
-
-5.times do
-  state = State.create()
-  dist = District.create(state: state)
-  Site.create(state: state, district: dist)
+list_provinces.each do |id, province_name|
+  State.create(id: id, state_name: province_name)
 end
+
+# require 'date'
+
+# 3.times do |t|
+#   Castle.all.each do |castle|
+#     random_day  = Random.new.rand(1..30)
+#     Night.create(castle_id: castle.id, not_available: Date.new(2015,06,random_day))
+#   end
+# end
+
+# Night.create(castle_id: 1, not_available: Date.new(2015,06,rand(1..30)))

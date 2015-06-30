93e8f85ecbd500f20a6be48fdb9595425a38f34c
Robert Johnston <rfjohnstonunicef@gmail.com>
2015-06-30 10:51:52 +0200

    corrected site view

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -7,7 +7,8 @@ class DistrictsController < ApplicationController
   def show
     @state = State.find(params[:state_id])
     @district = @state.districts.find(params[:id])
-    # @site = @state.district.sites.find(params[:id])
+
+    #@site = @state.district.sites.find(params[:id])
 
   end
 end

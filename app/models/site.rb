acb3b621e1eb0403a6b5971078d9d6c5c8bd8848
Robert Johnston <rfjohnstonunicef@gmail.com>
2015-06-30 18:07:58 +0200

    target populations

--- app/models/site.rb
+++ app/models/site.rb
@@ -24,6 +24,22 @@ class Site < ActiveRecord::Base
     percent = most_recent_population_report.read_attribute(population).to_f / most_recent_population_report.read_attribute(:total_population) * 100
   end
 
+  def vitared_target_pop
+    target_pop = most_recent_population_report.read_attribute(:child_population) * 0.1
+  end
+
+  def vitablue_target_pop
+    target_pop = most_recent_population_report.read_attribute(:child_population) * 0.8
+  end
+
+  def deworming_target_pop
+    target_pop = most_recent_population_report.read_attribute(:child_population) * 0.9
+  end
+
+  def iron_folate_target_pop
+    target_pop = most_recent_population_report.read_attribute(:woman_population) * 0.05
+  end
+
   def child_coverage(program)
     child_percent_coverage = program_reports_total(program) / most_recent_population_report(:child_population)
   end

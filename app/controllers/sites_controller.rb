11ab5d327222202efa44afc55a3ba2f8389ceba7
Robert Johnston <rfjohnstonunicef@gmail.com>
2015-07-02 12:25:42 +0200

    all changes for complete reporting on stocks

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -7,10 +7,6 @@ class SitesController < ApplicationController
   def show
     @site = Site.find(params[:id])
     @stock_reports_per_day = @site.stock_reports_per_day
-    # @complete_stock_report_vit_a_red = @site.stock_report_per_day(start_date, end_date, stock_item)
-    # @complete_stock_report_vit_a_blue = @site.stock_report_per_day(start_date, end_date, stock_item)
-    # @complete_stock_report_deworming = @site.stock_report_per_day(start_date, end_date, stock_item)
-    # @complete_stock_report_iron_folate = @site.stock_report_per_day(start_date, end_date, stock_item)
   end
 
 end

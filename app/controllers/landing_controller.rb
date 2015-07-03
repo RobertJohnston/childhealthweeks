a5fa2094d66da4abaf975be10d1ab755d25bf31a
Robert Johnston <rfjohnstonunicef@gmail.com>
2015-07-03 15:16:25 +0200

    added map with gmaps4rails

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,13 @@
 class LandingController < ApplicationController
   def home
+    @sites = Site.all
+
+    @gps_hash = Gmaps4rails.build_markers(@sites) do |site, marker|
+    marker.lat site.latitude
+    marker.lng site.longitude
+    end
+
+    # byebug
+
   end
 end
